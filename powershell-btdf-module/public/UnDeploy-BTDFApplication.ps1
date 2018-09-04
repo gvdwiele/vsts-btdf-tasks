73f0be71ea74866201d0ec71512102640c29ad28
Van de Wiele Gregory <gregory@eai.be>
2018-09-04 11:46:54 +0200

    re-added module

---
+++ powershell-btdf-module/public/UnDeploy-BTDFApplication.ps1
@@ -0,0 +1,85 @@
+﻿function Undeploy-BTDFApplication {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Name,
+        [Parameter(Mandatory)]
+        [string]$ProgramFilesDir,
+        [Parameter(Mandatory)]
+        [string]$ProgramName,
+        [string]$BTDeployMgmtDB = $true
+    )
+    Begin {
+        . ..\private\Init-BTDFTasks.ps1
+    }
+    Process {
+
+
+        if ([string]::IsNullOrWhiteSpace($ProgramFilesDir)) {
+            $ProgramFilesDir = $ProgramFiles
+        }
+        if ([string]::IsNullOrWhiteSpace($ProgramName)) {
+            $ProgramName = $Name
+        }
+        if (Test-BTDFApplicationDeployed -Name $Name) {
+            $ApplicationPath = Join-Path $ProgramFilesDir $ProgramName
+
+            $BTDFProject = Get-ChildItem -Path $ApplicationPath -Filter '*.btdfproj' -Recurse | Select-Object -ExpandProperty FullName -First 1
+            $DeployResults = Get-ChildItem -Path $ApplicationPath -Filter 'DeployResults' -Recurse | Select-Object -ExpandProperty FullName -First 1
+            if ($null -eq $DeployResults) {
+                Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not found." -f $ApplicationPath)
+            }
+            else {
+                $DeployResults = Join-Path $DeployResults 'DeployResults.txt'
+
+                $BTDFMSBuild = Get-MSBuildPath
+                $arguments = [string[]]@(
+                    "/l:FileLogger,Microsoft.Build.Engine;logfile=`"$DeployResults`""
+                    "/p:Configuration=Server"
+                    "/p:DeployBizTalkMgmtDB=$BTDeployMgmtDB"
+                    '/target:Undeploy'
+                    """$BTDFProject"""
+                )
+                $cmd = $BTDFMSBuild, ($arguments -join ' ') -join ' '
+                Write-Host $cmd
+                $exitCode = (Start-Process -FilePath "$BTDFMSBuild" -ArgumentList $arguments -Wait -PassThru).ExitCode
+                Write-Host (Get-Content -Path $DeployResults | Out-String)
+
+                if ($exitCode -ne 0) {
+                    Write-Host "##vso[task.logissue type=error;] Error while calling MSBuild, Exit Code: $exitCode"
+                    Write-Host ("##vso[task.complete result=Failed;] Deploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+                }
+                else {
+                    Write-Host "##vso[task.complete result=Succeeded;]DONE"
+                }
+            }
+        }
+        else {
+            Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not in catalog.  Undeploy skipped." -f $Name)
+        }
+    }
+    End
+    {}
+}
+
+
+function Test-BTDFApplicationDeployed {
+    param(
+        [Parameter(ValueFromPipeline = $true)]
+        [string[]]$Name
+    )
+    begin {
+        #=== Make sure the ExplorerOM assembly is loaded ===#
+        [void] [System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM")
+        #=== Connect the BizTalk Management database ===#
+        $Catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+        $MgmtDBServer = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\BizTalk Server\3.0\Administration' | Select-Object -ExpandProperty 'MgmtDBServer'
+        $Catalog.ConnectionString = "SERVER=$MgmtDBServer;DATABASE=BizTalkMgmtDb;Integrated Security=SSPI"
+    }
+    process {
+        #=== Loop through applications in the catalog trying to find a name match ===#
+        foreach ($app in $Name) {
+            $Catalog.Applications.Name -contains $app
+        }
+    }
+}
